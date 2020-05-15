b72650b78fe7245070ce878c34450c75fcd93470
Shelby S <3541579+sillyfatcat@users.noreply.github.com>
2020-05-14 19:06:39 -0700

    added setup files added manifest file

--- setup.py
+++ setup.py
@@ -4,11 +4,13 @@ setup(
     name='keebsound',
     version='0.0.1',
     packages=['keeb'],
-    url='',
-    license='',
+    url='https://github.com/sillyfatcat/keebsound',
+    license='MIT',
     author='Shelby Shum',
     author_email='sshum00@gmail.com',
     description='Keeb generates realistic keyboard sounds depending on the input',
+    keywords=['KEYBOARD', 'SOUND', 'MECHANICAL KEYBOARD'],
+    download_url='https://github.com/sillyfatcat/keebsound/archive/v0.1.tar.gz',
     install_requires=[
         'click',
         'playsound',
@@ -16,5 +18,18 @@ setup(
     ],
     entry_points={
         'console_scripts': ['keeb=keeb.main:main']
-    }
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Fun Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
 )
